d0736886dea6205da060acd302fb715c22146c0d
krishtimil <krishtimil@gmail.com>
2024-06-08 02:30:33 +0545

    added drawer

--- components/emailList.tsx
+++ components/emailList.tsx
@@ -7,25 +7,62 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer"
+
 import { IEmail } from 'gmail-api-parse-message-ts';
 
 
-const EmailCard  = ({...props}) => {
+const EmailCard = ({ ...props }) => {
   return (
     <Card>
       <CardHeader>
         <CardTitle>{props.email.subject}</CardTitle>
         <CardDescription>
           <div className='flex flex-row justify-between'>
-              <div>{props.email.from.name} | {props.email.from.email}</div>
-              <div>{new Date(props.email.sentDate).toDateString()}</div>
+            <div>{props.email.from.name} | {props.email.from.email}</div>
+            <div>{new Date(props.email.sentDate).toDateString()}</div>
           </div>
-          </CardDescription>
+        </CardDescription>
       </CardHeader>
       <CardContent>{props.email.snippet}</CardContent>
       <CardFooter>{props.email.id}</CardFooter>
     </Card>
   );
+}
+
+const EmailDrawer = ({ ...props }) => {
+  return (
+    <Drawer>
+      <DrawerTrigger>
+        <EmailCard {...props} />
+      </DrawerTrigger>
+      <DrawerContent>
+        
+          <DrawerHeader>
+            <DrawerTitle>{props.email.subject}</DrawerTitle>
+            <DrawerDescription >
+              <div className='flex flex-row justify-between'>
+                <div>{props.email.from.name} | {props.email.from.email}</div>
+                <div>{new Date(props.email.sentDate).toDateString()}</div>
+              </div>
+            </DrawerDescription>
+          </DrawerHeader>
+      
+
+        <p dangerouslySetInnerHTML={{ __html: props.email.textHtml }}></p>
+      </DrawerContent>
+    </Drawer>
+  );
+
 }
 
 
@@ -48,12 +85,12 @@ const EmailList = () => {
 
   return (
     <div className='space-y-10'>
-        {emails.length === 0 && <p>No emails found.</p>} 
-    
-        {emails.map((email) => (
-          <EmailCard email={email} key={email.id}/>
-        ))}
-      
+      {emails.length === 0 && <p>No emails found.</p>}
+
+      {emails.map((email) => (
+        <EmailDrawer email={email} key={email.id} />
+      ))}
+
     </div>
   );
 };
